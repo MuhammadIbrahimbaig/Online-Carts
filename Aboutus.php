a664c9aff8a48df071bf1d680f7d80fb8ecb852a
asfahan321 <138752769+asfahan321@users.noreply.github.com>
2023-12-13 19:37:53 +0500

    about set

--- Aboutus.php
+++ Aboutus.php
@@ -2,39 +2,6 @@
     include("header.php");
    ?>
 
-<main id="main">
- <!-- ======= About Section ======= -->
-
- <section id="about" class="about ">
-      <div class="container" data-aos="fade-up">
-
-        <div class="row">
-          <div class="col-lg-6 order-1 order-lg-2 mt-5" data-aos="fade-left" data-aos-delay="100">
-            <img src="images/about.jpeg" class="img-fluid" alt="">
-          </div>
-          <div class="col-lg-6 pt-4 pt-lg-0 order-2 order-lg-1 content mt-5" data-aos="fade-right" data-aos-delay="100">
-            <h3 class="mt-5">About Us</h3>
-            <p class="fst-italic">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
-              magna aliqua.
-            </p>
-            <ul>
-              <li><i class="ri-check-double-line"></i> Ullamco laboris nisi ut aliquip ex ea commodo consequat.</li>
-              <li><i class="ri-check-double-line"></i> Duis aute irure dolor in reprehenderit in voluptate velit.</li>
-              <li><i class="ri-check-double-line"></i> Ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate trideta storacalaperda mastiro dolore eu fugiat nulla pariatur.</li>
-            </ul>
-            <p>
-              <!-- Ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-              velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident -->
-            </p>
-          </div>
-        </div>
-
-      </div>
-    </section><!-- End About Section -->
-    
-       
-</main>
 
     <?php
 include("footer.php");
