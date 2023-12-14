831235ec14bb8448de2b9344dae17e74f3acc8e7
asfahan321 <138752769+asfahan321@users.noreply.github.com>
2023-12-14 07:23:02 +0500

    about us complete

--- Aboutus.php
+++ Aboutus.php
@@ -2,7 +2,34 @@
     include("header.php");
    ?>
 
+>
+    
+ <!-- about us  start-->
+ 
+    <div class="container mt-5 ">
+        <div class="row gx-5 gy-5">
+            <div class="col-md-6 col-xxl-6 col-lg-6 col-12 ">
+                <h1 class="mt-5 about_heading"><span class="welcometxt">Welcome!</span> Air conditioner</h1>
+                <p class="lh-3 about_paragraph">ELECTRONICS STORE: A retail establishment used for the selling or repairing of consumer electronic products such as home and car stereos, televisions, telephones, and personal computers Electronics Stores means stores that offer for sale primarily electronic products. Examples of Electronics Stores include, without limitation, CIRCUIT CITY, FRY'S and BEST BUY.
+                </p>
+            </div>
+            <!-- picture -->
+            <div class="col-md-6 col-xxl-6 col-lg-6 col-12  ">
+                <img src="images/watch8.jpg" alt="load error" class=" about_img_radius img-fluid"  >  
+        </div>
+        </div>
+    </div>
 
+   
+
+
+<!-- about us end-- -->
+
+
+
+
+</body>
+</html>
     <?php
 include("footer.php");
 ?>
