5a37a675075dc903903479303f1777ff7ae746d6
MuhammadIbrahimbaig <138751972+MuhammadIbrahimbaig@users.noreply.github.com>
2023-12-29 12:14:29 +0500

    add product

--- index.php
+++ index.php
@@ -15,7 +15,7 @@
                     <a href="shop.html" class="btn btn-medium btn-dark text-uppercase btn-rounded-none">Shop Product</a>
                   </div>
                 </div>
-                <div class="col-md-5">
+                  <div class="col-md-5">
                   <div class="image-holder">
                     <img src="images/banner-image.png" alt="banner">
                   </div>
@@ -147,16 +147,13 @@
               <div class="swiper-slide">
                 <div class="product-card position-relative">
                   <div class="image-holder">
-                    <img src="images/mobile1.webp" alt="product-item" class="img-fluid">
+                    <img src="images/Ap 02.png" alt="product-item" class="img-fluid">
                   </div>
                   <div class="cart-concern position-absolute">
-                    <!-- <div class="cart-button d-flex">
-                      <a href="#" class="btn btn-medium btn-black">Add to Cart<svg class="cart-outline"><use xlink:href="#cart-outline"></use></svg></a>
-                    </div> -->
                   </div>
                   <div class="card-detail d-flex justify-content-between pt-3">
                     <h3 class="card-title text-uppercase">
-                      <a href="#">black watch</a>
+                      <a href="#">Air Buds</a>
                     </h3>
                     <span class="item-price text-primary">$750</span>
                   </div>
---
+++ online_carts (1).sql
@@ -0,0 +1,238 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Dec 29, 2023 at 08:13 AM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `online_carts`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `cart`
+--
+
+CREATE TABLE `cart` (
+  `cart_id` int(11) NOT NULL,
+  `user_id` int(11) DEFAULT NULL,
+  `total_price` int(11) DEFAULT NULL,
+  `status` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `categories`
+--
+
+CREATE TABLE `categories` (
+  `Cat_Id` int(11) NOT NULL,
+  `Category` varchar(25) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `categories`
+--
+
+INSERT INTO `categories` (`Cat_Id`, `Category`) VALUES
+(1, 'Watches'),
+(2, 'Mobiles'),
+(3, 'airpods'),
+(4, 'hodiee'),
+(5, 'shoes');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `products`
+--
+
+CREATE TABLE `products` (
+  `prod_id` int(11) NOT NULL,
+  `prodname` varchar(100) NOT NULL,
+  `prodprice` int(11) NOT NULL,
+  `prodimage` varchar(255) NOT NULL,
+  `Prod_Desc` varchar(255) NOT NULL,
+  `Prod_Category` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `products`
+--
+
+INSERT INTO `products` (`prod_id`, `prodname`, `prodprice`, `prodimage`, `Prod_Desc`, `Prod_Category`) VALUES
+(46, 'Smart Watch', 4000, 'img/smartwatch.png', 'Mobile watch best quality', 1),
+(48, 'Mobile', 1900000, 'img/mobile1.webp', 'Smart phone best quality', 2),
+(49, 'Mobile', 34000, 'img/mobile2.webp', 'Smart phone best quality', 2),
+(50, 'Mobile', 50000, 'img/mobile3.webp', 'Smart phone best quality', 2),
+(51, 'smart watch', 6000, 'img/smartwatch2.webp', 'Buy Dany Loop Pro Smart Watch Price in Pakistan', 1),
+(52, 'Smart watch', 9000, 'img/smartwatch3.gif', 'Buy T10 Ultra Bluetooth Calling Watch Price in Pakistan', 1),
+(53, 'Smart watch', 9000, 'img/smartwatch4.webp', 'Buy S100 Pro Max Smart Watch With 4 Straps Price in Pakistan', 1),
+(55, 'Hodiee', 1300, 'img/01.png', 'Hoodies keep you warm and cozy! Update your wardrobe with our latest hoodies collection', 4),
+(56, 'Hodiee', 1500, 'img/02.png', 'Brandsego is the leading online store for men\'s hoodies in Pakistan.', 4),
+(57, 'Hodiee', 5000, 'img/03.png', ' Slim Fit Winter High Necks For Men | Full Sleeve Warm Winter', 4),
+(58, 'Hodiee', 4090, 'img/04.png', ' Slim Fit Winter High Necks For Men | Full Sleeve Warm Winter', 4),
+(59, 'Hodiee', 7990, 'img/05.png', ' Slim Fit Winter High Necks For Men | Full Sleeve Warm Winter', 4),
+(60, 'Hodiee', 2000, 'img/06.png', ' New fleece plain full sleves pull over winter hodiee for men ', 4),
+(61, 'Hodiee', 600, 'img/07.png', ' New fleece plain full sleves pull over winter hodiee for men ', 4),
+(62, 'Hodiee', 1400, 'img/08.png', ' New fleece plain full sleves pull over winter hodiee for men ', 4),
+(63, 'Hodiee', 4500, 'img/09.png', ' New fleece plain full sleves pull over winter hodiee for men ', 4),
+(64, 'Hodiee', 2000, 'img/10.png', ' New fleece plain full sleves pull over winter hodiee for men ', 4),
+(65, 'Hodiee', 2000, 'img/11.png', ' New fleece plain full sleves pull over winter hodiee for men ', 4),
+(66, 'Hodiee', 2500, 'img/12.png', ' New fleece plain full sleves pull over winter hodiee for men ', 4),
+(67, 'Hodiee', 2500, 'img/AP 01.png', ' Audionic Airbud 550 Quad MIC ENC Earbuds, Gaming Mode, IPx4', 3),
+(68, 'Airbuds', 7500, 'img/AP 02.png', ' Audionic Airbud 550 Quad MIC ENC Earbuds, Gaming Mode, IPx4', 3),
+(69, 'Airbuds', 500, 'img/AP 03.png', ' Audionic Airbud 550 Quad MIC ENC Earbuds, Gaming Mode, IPx4', 3);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `user_id` int(11) NOT NULL,
+  `user_name` varchar(255) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `passwordd` varchar(255) DEFAULT NULL,
+  `address` varchar(255) DEFAULT NULL,
+  `Phone` varchar(11) NOT NULL,
+  `role` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`user_id`, `user_name`, `email`, `passwordd`, `address`, `Phone`, `role`) VALUES
+(1, 'asfand', 'asfahan@gmail.com', '12345', 'home', '12345678988', 2),
+(2, 'wardah', 'xyz@gmail.com', 'abc123', 'karachi', '12345678901', 2),
+(3, 'Ibrahim', 'xyz@gmail.com', 'abc123', 'lahore', '12345678908', 2),
+(4, 'Ibrahim', 'xyz@gmail.com', 'abc123', 'lahore', '12345678908', 2),
+(5, 'asfand', 'xyz@gmail.com', '123', 'lahore', '12345678988', 2),
+(6, 'admin', 'admin@gmail.com', 'admin123', 'karachi', '03124567894', 1),
+(7, 'Mubsir', 'mubsir@gmail.com', 'mubsir', 'I block', '03172239904', 2),
+(8, 'sufi', 'sufi@gmail.com', 'sufi', 'bahria', '424224424', 2),
+(9, 'Arbaz', 'arbaz@gmail.com', '6767', 'aotech', '7654', 1),
+(10, 'talha', 'talha@gmail.com', 'talha', 'new', '98768868686', 2),
+(11, 'hina', 'hina@gmail.com', 'hina', '234567', '0987654', 2),
+(12, 'jannat', 'jannat@gmail.com', 'janat', 'home', '98765', 2),
+(13, 'anus', 'anus@gmail.com', 'anus', 'hme', '09876876', 2),
+(14, 'Imran', 'imran@gmail.com', 'imran', 'home', '0987654', 1),
+(15, 'niazi', 'nizazi@gmail.com', '234', '2345', '2334', NULL),
+(16, 'xubi', 'xubi@gmail.com', 'xubi', 'home', '098765', NULL),
+(17, 'Ibrahim', 'xyz@gmail.com', '112233', 'street', '12345678988', 2),
+(18, 'kuch bhiii', 'kuchbhi@gmail.com', 'kuchbhi', 'lahore', '090078601', 2);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_role`
+--
+
+CREATE TABLE `user_role` (
+  `role_id` int(11) NOT NULL,
+  `role_name` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `user_role`
+--
+
+INSERT INTO `user_role` (`role_id`, `role_name`) VALUES
+(1, 'Admin'),
+(2, 'User');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `categories`
+--
+ALTER TABLE `categories`
+  ADD PRIMARY KEY (`Cat_Id`);
+
+--
+-- Indexes for table `products`
+--
+ALTER TABLE `products`
+  ADD PRIMARY KEY (`prod_id`),
+  ADD KEY `Prod_Category` (`Prod_Category`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`user_id`),
+  ADD KEY `role` (`role`);
+
+--
+-- Indexes for table `user_role`
+--
+ALTER TABLE `user_role`
+  ADD PRIMARY KEY (`role_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `categories`
+--
+ALTER TABLE `categories`
+  MODIFY `Cat_Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `products`
+--
+ALTER TABLE `products`
+  MODIFY `prod_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=70;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+
+--
+-- AUTO_INCREMENT for table `user_role`
+--
+ALTER TABLE `user_role`
+  MODIFY `role_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `products`
+--
+ALTER TABLE `products`
+  ADD CONSTRAINT `products_ibfk_1` FOREIGN KEY (`Prod_Category`) REFERENCES `categories` (`Cat_Id`);
+
+--
+-- Constraints for table `users`
+--
+ALTER TABLE `users`
+  ADD CONSTRAINT `users_ibfk_1` FOREIGN KEY (`role`) REFERENCES `user_role` (`role_id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
